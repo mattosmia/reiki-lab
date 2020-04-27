4158eca6f12ac5eb003c3bb0088c13f4c311ad5e
carladgibin <56366941+carladgibin@users.noreply.github.com>
2020-04-27 19:48:32 +0100

    Update CookiePolicy.js

--- client/src/components/CookiePolicy.js
+++ client/src/components/CookiePolicy.js
@@ -7,7 +7,7 @@ import ContactForm from './_ContactForm';
 
 
 const contentCopy = <>
-					<p>This is the Cookie Policy for Reiki Lab Dublin Website, accessible from https://reikilabdublin.com/cookie-policy</p>
+			<p>This is the Cookie Policy for Reiki Lab Dublin Website, accessible from https://reikilabdublin.com/cookie-policy</p>
                         <p><strong>What Are Cookies</strong></p>
                         <p>As is common practice with almost all professional websites this site uses cookies, which are tiny files that are downloaded to your computer, to improve your experience. This page describes what information they gather, how we use it and why we sometimes need to store these cookies. We will also share how you can prevent these cookies from being stored however this may downgrade or 'break' certain elements of the sites functionality.</p>
                         <p>For more general information on cookies see the Wikipedia article on HTTP Cookies.</p>
