b2a15f0fb9193b1002f8ad1f0c3e0bd829127a9d
carladgibin <56366941+carladgibin@users.noreply.github.com>
2020-04-27 20:24:03 +0100

    Update PrivacyPolicy.js

--- client/src/components/PrivacyPolicy.js
+++ client/src/components/PrivacyPolicy.js
@@ -7,7 +7,7 @@ import ContactForm from './_ContactForm';
 
 
 const contentCopy = <>
-					<p>Effective date: April 22, 2020</p>
+			<p>Effective date: April 22, 2020</p>
                         <p>Reiki Lab App ("us", "we", or "our") operates the https://reikilabdublin.com/ website and the Reiki Lab App mobile application (the "Service").</p>
                         <p>This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data. Our Privacy Policy  for Reiki Lab App is created with the help of the <a href="https://www.freeprivacypolicy.com/free-privacy-policy-generator.php" target="_blank" rel="noopener noreferrer">Free Privacy Policy Generator</a>.</p>
                         <p>We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions.</p>
