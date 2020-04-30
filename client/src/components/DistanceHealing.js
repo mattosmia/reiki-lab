73a99c3fb6b8ff597e1a3b3b2815a2a74edd222e
carladgibin <56366941+carladgibin@users.noreply.github.com>
2020-04-30 17:37:50 +0100

    Update DistanceHealing.js

--- client/src/components/DistanceHealing.js
+++ client/src/components/DistanceHealing.js
@@ -13,7 +13,7 @@ const contentCopy = <>
 	<p>During the section try to look for a quiet place, play calm music, relax your mind and enjoy the flow of the Reiki energy.</p>
 	<p>Subscriptions will be held in our databases for a maximum of four weeks, therefore we advise everyone to subscribe to the sessions again every first day of a new month.</p>
 	<ul className="link-list unstyled-list">
-		<li><Link to="/events">For more information about our Reiki Sections, please visit our Events page.</Link></li>
+		<li><a href="https://www.eventbrite.com/o/reiki-lab-29215302519" target="_blank" rel="noopener noreferrer">For more information about our Reiki Sections, please visit our Events page.</a></li>
 	</ul>
 </>;
 
