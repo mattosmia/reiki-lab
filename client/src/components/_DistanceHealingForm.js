2c1ed3ce25ff7da9234f96471c3aa5c926f29ff7
mattosmia <mattosmia@gmail.com>
2020-05-01 01:33:54 +0100

    Added loading state to Distance healing form

--- client/src/components/_DistanceHealingForm.js
+++ client/src/components/_DistanceHealingForm.js
@@ -4,11 +4,13 @@ import axios from "axios";
 
 import FormValidation from './__FormValidation';
 import { returnCountriesObject, createDropdownOptions } from './__Utils';
+import Loading from './__Loading';
 
 class DistanceHealingForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			loading: true,
 			countriesList: [],
 			formSubmitted: false,
 			requestError: false,
@@ -42,6 +44,7 @@ class DistanceHealingForm extends Component {
 
 		this.setState({
 			countriesList: returnCountriesObject(),
+			loading: false
 		});
 	}
 
@@ -80,16 +83,19 @@ class DistanceHealingForm extends Component {
 
 	submitForm = () => {
 		this.setState({
+			loading: true,
 			formSubmitted: true,
 			requestError: false
 		}, () => {
 			axios.post('/distance-healing', this.state.formFieldValues)
 			.then(res => {
 				this.setState({
+					loading: false,
 					requestSuccess: true
 				})
 			}).catch(error => 
 				this.setState({
+					loading: false,
 					formSubmitted: false,
 					requestError: true
 				})
@@ -104,6 +110,8 @@ class DistanceHealingForm extends Component {
 					{ !this.state.requestSuccess &&
 					<>
 					<form noValidate className="form">
+						{ this.state.loading && <Loading /> }
+						{ ! this.state.loading && <>
 						{ this.state.requestError && <p className="error-message">Sorry, an error occurred. Please try again.</p> }
 						<label htmlFor="dhfFirstName">First name</label>
 						<input type="text" name="dhfFirstName" id="dhfFirstName" placeholder="First name *" onChange={this.handleChange} onBlur={this.handleBlur} />
@@ -122,6 +130,7 @@ class DistanceHealingForm extends Component {
 						<input type="checkbox" id="dhfConfirmList" name="dhfConfirmList" onChange={this.handleChange} onBlur={this.handleBlur} /><label className="inline-label" htmlFor="dhfConfirmList">Please, place me on this month's distance healing list *</label>
 						<input type="checkbox" id="dhfTerms" name="dhfTerms" onChange={this.handleChange} onBlur={this.handleBlur} /><label className="inline-label" htmlFor="dhfTerms">I agree with the <Link to="/terms-conditions" target="_blank" rel="noopener noreferrer">Terms & Conditions</Link> *</label>
 						<button type="button" className={`btn btn--secondary${this.state.formSubmitted? ' btn--waiting': ''}`} disabled={!this.state.formValid || this.state.formSubmitted} onClick={this.submitForm}>Add me to the list</button>
+						</>}
 					</form>
 					</>}
 					{ this.state.requestSuccess && <p className="success-message">Thank you for submitting your details. You are now on this month's Distance Healing list.</p> }
